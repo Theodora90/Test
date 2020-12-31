2720d90ccb119d43f44ccebfe8618161ca441647
Theodora90 <57830089+Theodora90@users.noreply.github.com>
2020-12-31 17:36:10 +0100

    Add files via upload

---
+++ file.js
@@ -0,0 +1,30 @@
+let button = document.getElementById("login");
+button.addEventListener("click", login);
+
+async function login(e) {
+    e.preventDefault();
+    let email = document.getElementById('exampleInputEmail1').value;
+    let password = document.getElementById('exampleInputPassword1').value;
+
+
+    let data = {
+        email: email,
+        password: password
+    };
+
+   let response = await fetch('https://fcs.concept-nova.com/api/v1/login ',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(data)
+    });
+
+    let result = await response.json();
+    console.log(result)
+
+    if (result.code == 200) {
+        localStorage.setItem('token', result.message.token);
+        window.location.replace('./index.html');
+    }
+}
